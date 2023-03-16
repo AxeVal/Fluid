08f195da7578fbfd6ee44bd67585aa1cec66558d
Valentin <119501691+AxeVal@users.noreply.github.com>
2023-03-16 14:13:21 +0700

    Add files via upload

---
+++ Tasks.py
@@ -0,0 +1,8 @@
+def hello():
+    return "hello"
+
+def main():
+    print(hello())
+
+if __name__ == '__main__':
+    main()
---
+++ test_hello.py
@@ -0,0 +1,4 @@
+from Tasks import hello
+
+def test_hello():
+    assert hello() == "hello"
