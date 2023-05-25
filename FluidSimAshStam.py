34055839808a008ed78f396809424e4b59ed9b9e
Valentin <119501691+AxeVal@users.noreply.github.com>
2023-05-25 12:45:20 +0700

    Update FluidSimAshStam.py

--- FluidSimAshStam.py
+++ FluidSimAshStam.py
@@ -3,6 +3,7 @@ import pygame as pg
 import sys
 import cv2
 
+
 WIDTH = HEIGHT = N
 FPS = 50
 
@@ -12,6 +13,7 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+
 def start():
     pg.init()
     pg.mixer.init()
@@ -19,38 +21,38 @@ def start():
     pg.display.set_caption("Fluid simulation")
     clock = pg.time.Clock()
 
-    fluid = FluidCube(1e-2, 1e-9 / 2, 1e-7)
+    fluid = FluidCube(1e-2, 1e-9, 1e-11)
 
-    running = True
     sc.fill(BLACK)
     pg.display.update()
 
-    surf = pg.surfarray.make_surface(cv2.resize(fluid.density, (WIDTH*SCALE, HEIGHT*SCALE)))
+    surf = pg.surfarray.make_surface(cv2.resize(fluid.density,
+                                                (WIDTH*SCALE,
+                                                 HEIGHT*SCALE)))
 
     flag = False
-    pos0 = (0, 0)
-    while 1:
+    while True:
         for i in pg.event.get():
-            if i.type == pg.MOUSEBUTTONDOWN:
+            if i.type == pg.KEYDOWN and i.key == pg.K_ESCAPE:
+                sys.exit()
+            elif i.type == pg.MOUSEBUTTONDOWN:
                 flag = True
                 pos0 = pg.mouse.get_pos()
             elif i.type == pg.MOUSEBUTTONUP:
                 flag = False
 
-
         if flag:
-            pressed = pg.mouse.get_pressed()
             pos = pg.mouse.get_pos()
-            # pg.draw.rect(sc, BLACK, (pos[0], pos[1], SCALE, SCALE))
-            fluid.addDensity(pos[0]//SCALE, pos[1]//SCALE, 1000)
-            fluid.addVelocity(pos[0]//SCALE, pos[1]//SCALE, (pos[0]-pos0[0])//SCALE, (pos[1]-pos0[1])//SCALE)
-            pg.display.update()
+            fluid.addDensity(pos0[0]//SCALE, pos0[1]//SCALE, 4000)
+            fluid.addVelocity(pos0[0]//SCALE, pos0[1]//SCALE,
+                              (pos[0]-pos0[0])//SCALE, (pos[1]-pos0[1])//SCALE)
 
         fluid.step()
-
-        pg.surfarray.blit_array(surf, cv2.resize(fluid.density, (WIDTH*SCALE, HEIGHT*SCALE)))
+        pg.surfarray.blit_array(surf, cv2.resize(fluid.density, (WIDTH*SCALE,
+                                                                 HEIGHT*SCALE)))
         sc.blit(surf, (0, 0))
         pg.display.update()
+
 
 def main():
     start()
